2e730f548288fc56afc9b2f9ed56d3c7bebfff81
Bridget Kienzle <bridgetkien@gmail.com>
2018-07-26 13:54:33 +0000

    automatically backed up by learn

--- index.rb
+++ index.rb
@@ -7,33 +7,39 @@ ingredients = [
 ]
 
 def sandwich_request(ingredients, order) 
-  # your code here!
+ingredients = [
+    ["rye", "sourdough", "baguette"],
+    ["ham", "salami", "turkey"],
+    ["swiss", "munster", "cheddar"],
+    ["mayo", "mustard", "tabasco"]
+]
 end
+sandwich_request(0, 2) 
 
 # example city_info hash
-cities = {
-  :new_york => { 
-    :mayor => "Bill DeBlasio",
-    :population => 8337000,
-    :website => "http://www.nyc.gov"
-  },
-  :los_angeles => { 
-    :mayor => "Eric Garcetti",
-    :population => 3884307,
-    :website => "http://www.lacity.org"
-  },
-  :miami => { 
-    :mayor => "Tomás Regalado",
-    :population => 419777,
-    :website => "http://www.miamigov.com"
-  },
-  :chicago => { 
-    :mayor => "Rahm Emanuel",
-    :population => 2695598,
-    :website => "http://www.cityofchicago.org/"
-  }
-}
+# cities = {
+#   :new_york => { 
+#     :mayor => "Bill DeBlasio",
+#     :population => 8337000,
+#     :website => "http://www.nyc.gov"
+#   },
+#   :los_angeles => { 
+#     :mayor => "Eric Garcetti",
+#     :population => 3884307,
+#     :website => "http://www.lacity.org"
+#   },
+#   :miami => { 
+#     :mayor => "Tomás Regalado",
+#     :population => 419777,
+#     :website => "http://www.miamigov.com"
+#   },
+#   :chicago => { 
+#     :mayor => "Rahm Emanuel",
+#     :population => 2695598,
+#     :website => "http://www.cityofchicago.org/"
+#   }
+# }
 
-def city_populations(cities)
-  # your code here!
-end
+# def city_populations(cities)
+#   # your code here!
+# end
